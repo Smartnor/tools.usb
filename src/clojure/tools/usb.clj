167c7a8df3054f32b0e25376243c4377e1cd722a
Andrew T. Flower <andrewtflower@gmail.com>
2016-03-04 15:40:15 -0500

    Added a usb pipe and a core.async listener

--- src/clojure/tools/usb.clj
+++ src/clojure/tools/usb.clj
@@ -1,5 +1,6 @@
 (ns clojure.tools.usb
-  (:import [javax.usb UsbHostManager UsbConst]))
+  (:import [javax.usb UsbHostManager UsbConst])
+  (:require [clojure.core.async :as async :refer [go chan <! >! <!! >!!]]))
 
 (defn services
   []
@@ -43,6 +44,22 @@
   [device num]
   (let [config (configuration device)]
     (.getUsbInterface config (byte num))))
+
+(defn pipe 
+  [iface endpoint-num]
+  (.getUsbPipe (.getUsbEndpoint iface (unchecked-byte endpoint-num))))
+
+(defn listen 
+  [in-pipe]
+  (let [err-chan (chan)
+        event-chan (chan)
+        listener (proxy [javax.usb.event.UsbPipeListener] []
+                   (errorEventOccurred [event]
+                                       (>!! err-chan (.getUsbException event)))
+                   (dataEventOccurred [event]
+                                      (>!! event-chan (.getData event))))]
+    (.addUsbPipeListener in-pipe listener)
+    event-chan))
 
 (defn dump
   []
